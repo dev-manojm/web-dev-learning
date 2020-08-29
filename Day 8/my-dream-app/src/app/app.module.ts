d0efc12c27ff479c22d591efe193e99897c0e246
Manoj More <31646290+dev-manojm@users.noreply.github.com>
2020-08-29 11:35:00 +0530

    Binding one way two way

--- Day 8/my-dream-app/src/app/app.module.ts
+++ Day 8/my-dream-app/src/app/app.module.ts
@@ -4,19 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MycompComponent } from './mycomp/mycomp.component';
-import { userData } from './shared/userData.service'
-import { deptData } from './shared/deptData.service'
+import { userData } from './shared/userData.service';
+import { deptData } from './shared/deptData.service';
+import { FormsModule } from '@angular/forms';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MycompComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MycompComponent, FavoriteComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [userData, deptData],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
